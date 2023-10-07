1e690fd318fc48c0a12d34cc284722703556fd7d
JuanJoseVillamizarCampus1 <juanjvs1409@gmail.com>
2023-10-06 22:40:45 -0500

    fix: :bug:

--- frontend/my-app/src/components/Home.jsx
+++ frontend/my-app/src/components/Home.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
 import Navbar from "./header/Navbar";
 import HomePage from "./contenidoHome/HomePage";
 const Home = () => {
@@ -8,43 +7,6 @@ const Home = () => {
         <Navbar />
         <HomePage/>
       </div>
-     
-  
-
-    // <div>
-    //   <h1>Página de Inicio - Delitos</h1>
-    //   <p>Bienvenido a la plataforma de denuncias de delitos.</p>
-    //   <div>
-    //     <Link to="/login">
-    //       <button>Iniciar Sesión</button>
-    //     </Link>
-    //   </div>
-    //   <div>
-    //     <Link to="/registro">
-    //       <button>Registrarse</button>
-    //     </Link>
-    //   </div>
-    //   <div>
-    //     <Link to="/denuncia-anonima">
-    //       <Button>Hacer Denuncia Anónima</Button>
-    //     </Link>
-    //   </div>
-    // </div>
-=======
-import HeaderBanner from '../components/banner/banner'
-import Header from '../components/header/Holi';
-const Home = () => {
-  return (
-    <div id="main-wrapper">
-      <Header/>
-      <div className="page-wrapper">
-          <div className="container-fluid">
-              <HeaderBanner/>
-          </div>  
-      </div>
-    </div>
->>>>>>> 04aa08e351cc3ea499d13b72c68236bb63794bb7
-  );
-};
+)}
 
 export default Home;
